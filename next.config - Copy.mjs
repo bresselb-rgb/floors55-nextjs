d128d6269ba3fbe0f99b667717571ab1f2499142
Byron <bresselb@gmail.com>
2026-07-31 15:51:39 -0700

    Updated for speed nex.config.mjs

---
+++ next.config - Copy.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**', // This allows all images from your Firebase storage bucket
+      },
+    ],
+  },
+};
+
+export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,39 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
-        pathname: '/**', // This allows all images from your Firebase storage bucket
+        pathname: '/**', 
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application
+        source: '/(.*)',
+        headers: [
+          {
+            // Prevents your site from being loaded inside an iframe (Clickjacking mitigation)
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            // Forces browsers to use HTTPS (HSTS policy)
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            // Prevents browsers from guessing the content type
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            // Cross-Origin-Opener-Policy (COOP)
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          }
+        ],
+      },
+    ];
   },
 };
 
